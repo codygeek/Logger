58773765fefae47cbe74a7d7a713f0514dc65c66
hardik aggarwal <hardikaggrwl228@gmail.com>
2019-06-29 11:22:33 +0530

    Loggin App

--- app/src/main/java/com/example/loginscreen/MainActivity.java
+++ app/src/main/java/com/example/loginscreen/MainActivity.java
@@ -1,59 +1,124 @@
 package com.example.loginscreen;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.HttpMethod;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.share.model.ShareHashtag;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity  extends AppCompatActivity {
+
     TextView txtStatus;
     LoginButton loginButton;
-    CallbackManager callbackManager;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+    private String firstName, lastName, email, birthday, gender;
+    private URL profilePicture;
+    private String userId;
+    private String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_main);
-        initializeControls();
-        loginfb();
-
-    }
-    private void initializeControls(){
         callbackManager = CallbackManager.Factory.create();
-        txtStatus = (TextView)findViewById(R.id.txtStatus);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-    }
-    private void loginfb(){
-        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setHeight(100);
+        loginButton.setTextColor(Color.WHITE);
+        loginButton.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+        loginButton.setCompoundDrawablePadding(0);
+        FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-            txtStatus.setText("Login Success\n"+loginResult.getAccessToken());           }
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.e(TAG, object.toString());
+                        Log.e(TAG, response.toString());
+
+                        try {
+                            userId = object.getString("id");
+                            profilePicture = new URL("https://graph.facebook.com/" + userId + "/picture?width=500&height=500");
+                            if (object.has("first_name"))
+                                firstName = object.getString("first_name");
+                            if (object.has("last_name"))
+                                lastName = object.getString("last_name");
+                            if (object.has("email"))
+                                email = object.getString("email");
+                            if (object.has("birthday"))
+                                birthday = object.getString("birthday");
+                            if (object.has("gender"))
+                                gender = object.getString("gender");
+
+                            Intent main = new Intent(MainActivity.this, MainActivity.class);
+                            main.putExtra("name", firstName);
+                            main.putExtra("surname", lastName);
+                            main.putExtra("imageUrl", profilePicture.toString());
+                            startActivity(main);
+                            finish();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                //Here we put the requested fields to be returned from the JSONObject
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id, first_name, last_name, email, birthday, gender");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
 
             @Override
             public void onCancel() {
-            txtStatus.setText("Login cancelled");
             }
 
             @Override
-            public void onError(FacebookException error) {
-            txtStatus.setText("Eroor"+error.getMessage());
+            public void onError(FacebookException e) {
+                e.printStackTrace();
             }
-        });
+
+        };
+        loginButton.setReadPermissions("email", "user_birthday", "user_posts");
+        loginButton.registerCallback(callbackManager, callback);
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        super.onActivityResult(requestCode, responseCode, intent);
+        callbackManager.onActivityResult(requestCode, responseCode, intent);
     }
 }
